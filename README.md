e37b5cae6fbf4ca09ccfd0f918c0b020f8efc355
codebycorey <me@coreyodonnell.com>
2022-12-07 21:28:44 -0500

    day7

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -104,16 +104,16 @@ Both parts: 1.416083ms
 
 ```
 Day 06
-Time part 1: 0.299ms
-Time part 2: 2.475ms
-Both parts: 2.773417ms
+Time part 1: 0.535ms
+Time part 2: 2.372ms
+Both parts: 2.9061250000000003ms
 ```
 
 ```
 Day 07
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.645ms
+Time part 2: 7.171ms
+Both parts: 7.815708ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 16.581ms
+Total stars: 14/50
+Total time: 24.53ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day07/index.ts
@@ -0,0 +1,161 @@
+import run from "aocrunner";
+
+type FileNode = {
+  value: { name: string; size: number };
+  parent: FileNode | null;
+  children: FileNode[];
+};
+
+const createNode = (
+  value: FileNode["value"],
+  parent: FileNode | null,
+): FileNode => ({
+  value,
+  parent,
+  children: [],
+});
+
+class FileTree {
+  private head: FileNode;
+  private current: FileNode;
+
+  public constructor(input: string[]) {
+    this.head = createNode({ name: "/", size: 0 }, null);
+    this.current = this.head;
+
+    for (let i = 0; i < input.length; i++) {
+      const item = input[i].split(" ");
+      if (item[0] === "$") {
+        if (item[1] === "cd") {
+          this.cd(item[2]);
+        }
+      } else if (item[0] === "dir") {
+        this.addFileOrDir(item[1]);
+      } else {
+        this.addFileOrDir(item[1], parseInt(item[0]));
+      }
+    }
+  }
+
+  public cd(value: string): void {
+    if (value === "/") {
+      this.current = this.head;
+      return;
+    }
+
+    const current = this.current;
+    if (value === ".." && current.parent) {
+      this.current = current.parent;
+      return;
+    }
+
+    const dir = current.children.find((child) => child.value.name === value);
+    if (!dir) {
+      return;
+    }
+    this.current = dir;
+  }
+
+  public addFileOrDir(name: string, size: number = 0): void {
+    const node = createNode({ name, size }, this.current);
+    this.current.children.push(node);
+    if (size) {
+      this.addFileSizeToAllParents(node, size);
+    }
+  }
+
+  public addSizeForAllDirLessThanMax(max: number, node: FileNode = this.head) {
+    let total = 0;
+    if (!node.children.length) {
+      return total;
+    }
+
+    if (node.value.size < max) {
+      total += node.value.size;
+    }
+
+    for (let i = 0; i < node.children.length; i++) {
+      total += this.addSizeForAllDirLessThanMax(max, node.children[i]);
+    }
+    return total;
+  }
+
+  public findDirToDelete(
+    node: FileNode = this.head,
+    nodeToDelete: FileNode = this.head,
+  ) {
+    if (!node.children.length) {
+      return nodeToDelete;
+    }
+
+    const space = 30000000 - (70000000 - this.head.value.size);
+
+    if (node.value.size < nodeToDelete.value.size && node.value.size > space) {
+      nodeToDelete = node;
+    }
+
+    for (let i = 0; i < node.children.length; i++) {
+      const possibleNodeToDelete = this.findDirToDelete(
+        node.children[i],
+        nodeToDelete,
+      );
+      if (
+        possibleNodeToDelete.value.size < nodeToDelete.value.size &&
+        possibleNodeToDelete.value.size > space
+      ) {
+        nodeToDelete = possibleNodeToDelete;
+      }
+    }
+
+    return nodeToDelete;
+  }
+
+  private addFileSizeToAllParents(node: FileNode, size: number): void {
+    const parent = node.parent;
+    if (parent) {
+      parent.value.size += size;
+      this.addFileSizeToAllParents(parent, size);
+    }
+  }
+}
+
+const parseInput = (rawInput: string) => rawInput.split("\n");
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const fileTree = new FileTree(input);
+
+  return fileTree.addSizeForAllDirLessThanMax(100000);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const fileTree = new FileTree(input);
+
+  return fileTree.findDirToDelete().value.size;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
