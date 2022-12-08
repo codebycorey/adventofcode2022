46ca02741a853336a42258888e5a2e6c10f3097b
codebycorey <me@coreyodonnell.com>
2022-12-07 21:28:43 -0500

    day2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 ![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: -
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.922ms
+Time part 2: 0.732ms
+Both parts: 1.653167ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 0/50
-Total time: 0ms
+Total stars: 2/50
+Total time: 1.653ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.ts
@@ -0,0 +1,123 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split("\n");
+
+// A = Rock, B = Paper, C = Scissors
+// X = Rock, Y = Paper, Z = Scissors
+
+type Elf = "A" | "B" | "C";
+type Player = "X" | "Y" | "Z";
+type Outcome = Player;
+
+const mapPlayers: Record<Elf, Player> = {
+  A: "X",
+  B: "Y",
+  C: "Z",
+};
+
+const mapWinner: Record<Elf, Player> = {
+  A: "Y",
+  B: "Z",
+  C: "X",
+};
+
+const mapPlayValue: Record<Player, number> = {
+  X: 1,
+  Y: 2,
+  Z: 3,
+};
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  let score = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const [elf, me] = input[i].split(" ") as [Elf, Player];
+
+    let round = mapPlayValue[me];
+    if (mapWinner[elf] === me) {
+      round += 6;
+    } else if (mapPlayers[elf] === me) {
+      round += 3;
+    }
+
+    score += round;
+  }
+
+  return score;
+};
+
+// A = Rock, B = Paper, C = Scissors
+// X = Rock, Y = Paper, Z = Scissors
+const loseMap: Record<Elf, Player> = {
+  A: "Z",
+  B: "X",
+  C: "Y",
+};
+
+const drawMap: Record<Elf, Player> = {
+  A: "X",
+  B: "Y",
+  C: "Z",
+};
+
+const winMap: Record<Elf, Player> = {
+  A: "Y",
+  B: "Z",
+  C: "X",
+};
+
+const outcomeMap: Record<Outcome, Record<Elf, Player>> = {
+  X: loseMap,
+  Y: drawMap,
+  Z: winMap,
+};
+
+const outcomeValueMap: Record<Outcome, number> = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  let score = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const [elf, outcome] = input[i].split(" ") as [Elf, Outcome];
+
+    const player = outcomeMap[outcome][elf];
+    const outcomeValue = outcomeValueMap[outcome];
+    const playValue = mapPlayValue[player];
+
+    // console.log(outcomeValue, playValue);
+    score += outcomeValue + playValue;
+  }
+
+  return score;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
